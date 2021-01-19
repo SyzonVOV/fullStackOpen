c644863c6dd9d4f9301b0aa6bda9d4194129d9fe
SyzonVOV <sizonenko89@gmail.com>
2021-01-19 20:15:01 +0430

    7.8 done

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -1,25 +1,26 @@
-  
-import React, { useState, useEffect } from 'react'
-import {useField} from './CostumHooks/Hooks'
-
+import React from 'react';
+import { useField, useResource } from './CostumHooks/Hooks';
 
 const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+  const content = useField('text');
+  const name = useField('text');
+  const number = useField('text');
 
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
+  const [notes, noteService] = useResource('http://localhost:3005/notes');
+  const [persons, personService] = useResource('http://localhost:3005/persons');
 
-  const handleNoteSubmit = (event) => {
-    event.preventDefault()
-    noteService.create({ content: content.value })
-  }
- 
-  const handlePersonSubmit = (event) => {
-    event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
-  }
+  const handleNoteSubmit = event => {
+    event.preventDefault();
+    noteService.create({ content: content.value });
+    content.onChange()
+  };
+
+  const handlePersonSubmit = event => {
+    event.preventDefault();
+    personService.create({ name: name.value, number: number.value });
+    name.onChange()
+    number.onChange()
+  };
 
   return (
     <div>
@@ -28,17 +29,23 @@ const App = () => {
         <input {...content} />
         <button>create</button>
       </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map(n => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map(n => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part7/ultimate-hooks/src/CostumHooks/Hooks.js
+++ part7/ultimate-hooks/src/CostumHooks/Hooks.js
@@ -1,38 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export const useField = (type) => {
-  const [value, setValue] = useState('')
+export const useField = type => {
+  const [value, setValue] = useState('');
 
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
+  const onChange = event => {
+    setValue(event?.target.value||'');
+  };
 
   return {
     type,
     value,
-    onChange
-  }
-}
+    onChange,
+  };
+};
 
+export const useResource = baseUrl => {
+  const [resources, setResources] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(baseUrl);
+        console.log(
+          `🚀 ~ file: App.js ~ line 37 ~ useEffect ~ data`,
+          response.data,
+        );
+        setResources(response.data);
+      } catch (error) {
+        const { response } = error;
+        const { request, ...errorObject } = response;
+        console.log(errorObject);
+      }
+    }
+    fetchData();
+  }, [baseUrl]);
 
-
-
-export const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
-
-  // ...
-
-  const create = (resource) => {
-    // ...
-  }
+  const create = async resource => {
+    const response = await axios.post(baseUrl, resource)
+    console.log(`🚀 ~ file: Hooks.js ~ line 41 ~ response`, response.data);
+    setResources([...resources, response.data])
+  };
 
   const service = {
-    create
-  }
+    create,
+  };
 
-  return [
-    resources, service
-  ]
-}
+  return [resources, service];
+};
